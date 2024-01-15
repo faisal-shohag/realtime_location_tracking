4c30bad61fcec9d273ab7a6e5548f576f8a25b94
faisal-shohag <faisalshohagprog@gmail.com>
2024-01-16 00:19:57 +0600

    added option parameter to watchPosition function

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
 // get device lat and long
 const loc = document.getElementById("location")
 
+
+// initializing
+var map = L.map('map').setView([25.7181848, 89.2631757], 13)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>|Faisal'
+}).addTo(map)
+
+let marker;
+let circle;
+let zoomed;
+
+
 ok = (position) => {
     console.log("New location found!")
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const acc = position.coords.accuracy
     console.log(`lat: ${lat}\nlon: ${lon}`)
     loc.innerHTML = `lat: ${lat}<br>lon: ${lon}`
+    
+    if(marker) {
+        map.removeLayer(marker)
+    }
+    marker = L.marker([lat, lon]).addTo(map)
 }
 
 error = (err) => {
@@ -20,4 +40,3 @@ options = {
 
 let id = navigator.geolocation.watchPosition(ok, error, options)
 console.log(id)
-
